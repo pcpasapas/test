47ba5f89104eafed594042194974a0cb68d450e3
pcpasapas <pcpasapas@gmail.com>
2022-01-30 09:37:56 +0100

    30/01 9:37

--- src/components/compannonce.vue
+++ src/components/compannonce.vue
@@ -1,38 +1,24 @@
 
+<script>
 
+   export default {
+       name:"compannonce",
 
-<script >
-
-     export default {
-    name: 'compannonce', 
-    props: {
-        
-    },
-    data () {
-      return {
-
-      }
-    },
-    methods: {
-        
-    
-    },
-    created() {
-        
-    },
-} 
+       methods: {
+       },
+   }
 </script>
-
 <template>
-    <div>
-        <h4>Annonces</h4>  
-        <amp-ad width="100vw" height="320"
+    
+<!-- annonce -->
+<div class="amp-ad" width="100vw" height="320"
      type="adsense"
      data-ad-client="ca-pub-2401412736769800"
      data-ad-slot="3185461182"
      data-auto-format="rspv"
      data-full-width="">
   <div overflow=""></div>
-</amp-ad>
-    </div>
+</div>
+
 </template>
+
